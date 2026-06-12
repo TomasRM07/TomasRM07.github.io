b49b9ae82545b9d74bfb818cfcebe33e562ac5e0
TomasRM07 <tomasrmarrero07@gmail.com>
2026-06-12 15:16:56 +0100

    Actualizacion 2.1

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    if (btn && nav) {
+        btn.addEventListener("click", () => {
+            nav.classList.toggle("active");
+        });
+    }
+});
